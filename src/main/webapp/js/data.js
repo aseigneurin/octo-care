6f509440570bd32fc176fd16f079bff439c943fb
Alexis Seigneurin <seigneurin@idm.fr>
2013-09-21 00:30:34 +0100

    - more data

--- src/main/webapp/js/data.js
+++ src/main/webapp/js/data.js
@@ -16,17 +16,17 @@ var octoDetails = {
     "73408e49-d940-4de4-9798-c75ac4b09bd1": {
         name: "Octo 3",
         history: [
-            { text: "Et si on allait..." },
-            { text: "Tiens, du lorem ipsum..." },
-            { text: "C'est chouette..." }
+            { date: 1338323623006, text: "Et si on allait..." },
+            { date: 1388323123006, text: "Tiens, du lorem ipsum..." },
+            { date: 1318323623816, text: "C'est chouette..." }
         ]
     },
     "5a64eac0-e718-4513-8d47-8d446e26e6c9": {
         name: "Octo 1",
         history: [
-            { text: "Wahou..." },
-            { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac dui tempor, iaculis felis non, dignissim magna. Suspendisse facilisis rutrum eros, ut condimentum tortor tempus eget. Nulla facilisi. Nulla scelerisque tempor commodo. Sed quis lorem id ante imperdiet cursus. Praesent rutrum lacus eu vestibulum ullamcorper. Donec convallis, dolor eu venenatis interdum, libero sapien egestas nisl, quis ornare risus diam nec sapien. Suspendisse lectus orci, lobortis sed enim eget, gravida luctus erat. Etiam sit amet interdum tellus. Duis sed tristique magna. Curabitur suscipit elit nec sapien interdum laoreet. Nam commodo metus erat, vehicula pretium metus pulvinar at. Nam ante lorem, consectetur tristique arcu non, pulvinar cursus ligula. Praesent fermentum risus eu dui laoreet adipiscing." },
-            { text: "Hé ho !" }
+            { date: 1338323623006, text: "Wahou..." },
+            { date: 1388323123006, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac dui tempor, iaculis felis non, dignissim magna. Suspendisse facilisis rutrum eros, ut condimentum tortor tempus eget. Nulla facilisi. Nulla scelerisque tempor commodo. Sed quis lorem id ante imperdiet cursus. Praesent rutrum lacus eu vestibulum ullamcorper. Donec convallis, dolor eu venenatis interdum, libero sapien egestas nisl, quis ornare risus diam nec sapien. Suspendisse lectus orci, lobortis sed enim eget, gravida luctus erat. Etiam sit amet interdum tellus. Duis sed tristique magna. Curabitur suscipit elit nec sapien interdum laoreet. Nam commodo metus erat, vehicula pretium metus pulvinar at. Nam ante lorem, consectetur tristique arcu non, pulvinar cursus ligula. Praesent fermentum risus eu dui laoreet adipiscing." },
+            { date: 1318323623816, text: "Hé ho !" }
         ]
     }
 };
