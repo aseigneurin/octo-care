347caf10e320968de4a6af9673c610c4d84cd9db
Alexis Seigneurin <alexis@seigneurin.com>
2013-10-07 00:04:14 +0200

    - manage Octos through the API

--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -16,18 +16,33 @@ angular.module("octoCare", ["ui.bootstrap"])
         }
     ]);
 
-var OctoListCtrl = function($scope) {
-    $scope.octos = octos;
+var OctoListCtrl = function($scope, $http) {
+
+    $http.get("api/octo").success(function(data) {
+        $scope.octos = data;
+    }).error(function(data, status, headers, config) {
+        alert("Failed loading the Octos...");
+    });
 
     $scope.openDetail = function(octo) {
         window.location = "#/octo/" + octo.id;
     };
 };
 
-var OctoDetailCtrl = function($scope, $routeParams, $modal) {
+var OctoDetailCtrl = function($scope, $routeParams, $http, $modal) {
     $scope.octoId = $routeParams.octoId;
-    $scope.octo = octoDetails[$scope.octoId];
-    $scope.history = $scope.octo.history;
+
+    $http.get("api/octo/" + $scope.octoId).success(function(data) {
+        $scope.octo = data;
+    }).error(function(data, status, headers, config) {
+        alert("Failed loading the Octo...");
+    });
+
+    $http.get("api/octo/" + $scope.octoId + "/history").success(function(data) {
+        $scope.history = data;
+    }).error(function(data, status, headers, config) {
+        alert("Failed loading the history...");
+    });
 
     $scope.createEvent = function() {
         var modalInstance = $modal.open({
@@ -36,11 +51,21 @@ var OctoDetailCtrl = function($scope, $routeParams, $modal) {
         });
 
         modalInstance.result.then(function(result) {
-            $scope.history.push({
+            var data = {
+                text: result.text
+            };
+            $http.post("api/octo/" + $scope.octoId + "/history", data).success(function(data) {
+                data.highlight = "new";
+                console.log(data);
+                $scope.history.push(data);
+            }).error(function(data, status, headers, config) {
+                alert("Failed saving the event...");
+            });
+            /*$scope.history.push({
                 date: new Date(),
                 text: result.text,
                 highlight: "new"
-            });
+            });*/
         });
     };
 };
